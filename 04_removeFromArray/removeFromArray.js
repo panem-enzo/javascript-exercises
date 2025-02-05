59bbcf0ae7fe58e674ad9dc4af5fe1bf9e5a582d
Enzo Panem <panemjf@gmail.com>
2025-02-05 15:27:36 -0500

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
+    numItems = arguments.length;
+    array = arguments[0];
+    for(i=1; i<numItems; i++) {
+        while (array.indexOf(arguments[i]) != -1) {
+            removeIndex = array.indexOf(arguments[i]);
+            array.splice(removeIndex, 1);
+        }
+    }
+    return array;
 };
+
+console.log(removeFromArray([1, 2, 2, 5, 3, 4], 1, 2, 4));
 
 // Do not edit below this line
 module.exports = removeFromArray;
