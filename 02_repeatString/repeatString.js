9976851b9f83b9a6ae709e5879463aad1e59eed1
Enzo Panem <panemjf@gmail.com>
2025-02-05 13:54:00 -0500

    Completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    } else {
+        output = "";
+        for(i=0;i<num;i++) {
+            output += string;
+        }
+        return output;
+    }
 };
 
 // Do not edit below this line
