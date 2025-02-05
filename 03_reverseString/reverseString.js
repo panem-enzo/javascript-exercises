a8632017ddb7d96eb33faf319fffda7872f1aca4
Enzo Panem <panemjf@gmail.com>
2025-02-05 14:05:51 -0500

    Completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringArray = string.split("");
+    reversedArray = []
+    for (i=stringArray.length-1;i>-1;i--) {
+        reversedArray.push(stringArray[i]); 
+    }
+    return reversedArray.join("");
 };
+
+console.log(reverseString('hey there!'));
 
 // Do not edit below this line
 module.exports = reverseString;
