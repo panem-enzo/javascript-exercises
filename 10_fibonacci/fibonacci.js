727c9e85f4744408cd7ecf8cbbdb72a3a146aa8a
panem-enzo <epanem@my.yorku.ca>
2025-02-19 22:38:46 -0500

    Completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    arr = [1, 1];
+    num = parseInt(num);
+    if (num < 0) {
+        return "OOPS"
+    } else if (num === 0) {
+        return 0;
+    } else if (num === 1 || num === 2) {
+        return 1;
+    } else {
+        for (i=2; i < num; i++) {
+            arr.push(arr[i-2]+arr[i-1]);
+        }
+    }
+    return arr.pop();
 };
 
 // Do not edit below this line
