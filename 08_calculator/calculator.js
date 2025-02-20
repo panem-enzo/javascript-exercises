7ec7a0a3619b8378e20454bc33b0cb3bb807f1f6
panem-enzo <epanem@my.yorku.ca>
2025-02-19 21:55:46 -0500

    Completed 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
 const add = function() {
-	
+	return arguments[0]+arguments[1];
 };
 
 const subtract = function() {
-	
+	return arguments[0]-arguments[1];
 };
 
 const sum = function() {
-	
+	arr = arguments[0];
+  return arr.reduce((prev, item) => prev+item, 0);
 };
 
 const multiply = function() {
-
+  arr = arguments[0];
+  return arr.reduce((prev, item) => prev*item, 1);
 };
 
 const power = function() {
-	
+	return Math.pow(arguments[0], arguments[1]);
 };
 
 const factorial = function() {
-	
+	num = arguments[0];
+  output = 1;
+  for(i=1; i<=num; i++) {
+    output *= i;
+  }
+  return output;
 };
 
 // Do not edit below this line
