311b77cd6228f915aebc8981d3ee7bd48013a8e6
panem-enzo <epanem@my.yorku.ca>
2025-02-19 22:18:56 -0500

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().split(" ");
+    str = str.join("");
+    str = str.split(",");
+    str = str.join("");
+    // Two pointer
+    i = 0;
+    j = str.length-1;
+    while (i < j) {
+        if (str[j] === "!" || str[j] === ".") {
+            j--;
+        }
+        if (str[i] !== str[j]) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
